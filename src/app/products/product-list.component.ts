4ea3fca06e172f5b8eef6186ec5dfc878fc80f34
Jacob Adams <Jacob.Adams@neudesic.com>
2019-08-27 10:39:37 -0700

    create service to manage data

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-products',
@@ -21,44 +22,9 @@ export class ProductListComponent implements OnInit {
     this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
   }
   filteredProducts: IProduct[];
-  products: IProduct[] = [
-    {
-      productId: 2,
-      message: 'Hello',
-      productName: 'Garden Cart',
-      productCode: 'GDN-0023',
-      releaseDate: 'March 18, 2019',
-      description: '15 gallon capacity garden cart',
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl: 'N/A'
-    },
-    {
-      productId: 5,
-      message: 'git push --force master',
-      productName: 'Hammer',
-      productCode: 'TBX-0048',
-      releaseDate: 'May 21, 2019',
-      description: '21oz Framing Hammer',
-      price: 26.99,
-      starRating: 4.8,
-      imageUrl: 'N/A 2.0'
-    },
-    {
-      productId: 6,
-      message: 'git pull',
-      productName: 'Nail Puller',
-      productCode: 'TBX-0049',
-      releaseDate: 'May 15, 2019',
-      description: '9" Cats Paw',
-      price: 14.99,
-      starRating: 3.1,
-      imageUrl: 'N/A 2.0'
-    }
-  ];
+  products: IProduct[] = [];
 
-  constructor() {
-    this.filteredProducts = this.products;
+  constructor(private productService: ProductService) {
     this.listFilter = '';
   }
   onRatingClicked(message: string): void {
@@ -72,7 +38,9 @@ export class ProductListComponent implements OnInit {
   toggleImage(): void {
     this.showImage = !this.showImage;
   }
+  // on init, load this data (useful for pulling in data from Service / or DB)
   ngOnInit(): void {
-    console.log('in OnInit');
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
   }
 }
